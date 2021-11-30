2249da4e38f5f3002b26380d3b9558881bef22da
Alon-Elisha <alonmeelisha755.gmail.com>
2021-11-30 16:05:37 +0200

    adding main file and making my_mat a library

--- my_mat.c
+++ my_mat.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "my_mat.h"
 
-const int n = 3;
+
+#define n 3
 
 void getmatrix(int matrix1[n][n])
 {
@@ -30,23 +32,3 @@ void pathwayexist(int matrix[n][n], int i, int j)
 }
 
 
-int
-main (void)
-{
-  //printf ("Hello, world!\n");
-  int i,j, mat[n][n];
-  getmatrix(mat);
-  printf("Two Dimensional array elements:\n");
-  for(i=0; i<n; i++) {
-    for(j=0;j<n;j++) {
-        printf("%d ", mat[i][j]);
-        if(j==3){
-          printf("\n");
-        }
-      }
-  }
-  printf("\n");
-  pathwayexist(mat,1,2);
-  printf("\n");
-  
-}
---
+++ my_mat.h
@@ -0,0 +1,6 @@
+#define n 3
+
+void getmatrix(int matrix1[n][n]);
+
+void pathwayexist(int matrix[n][n], int i, int j);
+
